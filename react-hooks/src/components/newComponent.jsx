82bc6e0b8af518788a8133f009cdb13c75cb8eff
varunbm <bmvnaik3@gmail.com>
2020-04-03 15:34:41 +0530

    Changes

---
+++ react-hooks/src/components/newComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class newComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p>New Component</p>
+      </div>
+    );
+  }
+}
+
+export default newComponent;
